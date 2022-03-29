1dc0fce2daba84eb7d548658166efa90b42f8b12
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-03-29 10:51:41 +0200

    Correct "messages" with "chats"

--- src/tray-manager.ts
+++ src/tray-manager.ts
@@ -62,7 +62,7 @@ export default class TrayManager {
         }
 
         this.tray.setContextMenu(menu);
-        this.tray.setToolTip('WhatsApp Desktop - ' + this.unread + ' unread messages');
+        this.tray.setToolTip("WhatsApp Desktop - " + this.unread + " unread chats");
     }
 
     private onClickFirstItem() {

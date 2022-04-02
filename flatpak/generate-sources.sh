2965517af2ca5ec52d86333a5a64ab2138706a64
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-04-02 13:46:07 +0200

    Move flatpak-builder-tools into flatpak/

--- flatpak/generate-sources.sh
+++ flatpak/generate-sources.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=$(dirname "$0")
 
-GENERATOR_PATH="$DIR/../flatpak-builder-tools/node/flatpak-node-generator.py"
+GENERATOR_PATH="$DIR/flatpak-builder-tools/node/flatpak-node-generator.py"
 LOCK_FILE="$DIR/../package-lock.json"
 OUTPUT="$DIR/generated-sources.json"
 

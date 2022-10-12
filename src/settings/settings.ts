a58769d4470e8dfb104f69dafb22e9c14bc1f961
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-10-12 19:59:58 +0200

    refactor: make Settings not abstract

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,18 +1,18 @@
 import Store from 'electron-store';
 
-export default abstract class Settings {
+export default class Settings {
     private readonly store = new Store();
     private readonly section: string;
 
-    protected constructor(section: string) {
+    constructor(section: string) {
         this.section = section + ".";
     }
 
-    protected get(key: string, defaults: any = null): any {
+    public get(key: string, defaults: any = null): any {
         return this.store.get(this.section + key, defaults);
     }
 
-    protected set(key: string, value: any): void {
+    public set(key: string, value: any): void {
         this.store.set(this.section + key, value);
     }
 };
